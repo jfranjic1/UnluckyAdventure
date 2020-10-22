cf67e77d1f95026c9208f3195e483df0f8672dab
jfranjic1 <jfranjic1@etf.unsa.ba>
2020-10-22 10:25:46 +0200

    Hello world

---
+++ cmake-build-debug/CMakeFiles/UnluckyAdventure.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/UnluckyAdventure.dir/main.cpp.obj"
+  "UnluckyAdventure.exe"
+  "UnluckyAdventure.exe.manifest"
+  "UnluckyAdventure.lib"
+  "UnluckyAdventure.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/UnluckyAdventure.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/UnluckyAdventure.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for UnluckyAdventure.
+# This may be replaced when dependencies are built.
